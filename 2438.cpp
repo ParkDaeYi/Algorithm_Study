321f766063c0758689cc654b82e9127a1059ee33
ParkDaeYi <qdjjd@naver.com>
2020-02-21 22:18:55 +0900

    별 찍기-1

---
+++ 2438.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j <= i; ++j) cout << '*';
+		cout << '\n';
+	}
+	return 0;
+}
