9c0020386487bf68dafe96a36d5a3b27da6aa4ad
ParkDaeYi <qdjjd@naver.com>
2020-02-21 20:53:45 +0900

    N과 M(5)

---
+++ 15654.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m, arr[9], ans[9];
+bool visited[9];
+
+void backt(int);
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) cin >> arr[i];
+	sort(arr, arr + n);
+	backt(0);
+	return 0;
+}
+
+void backt(int depth) {
+	if (depth == m) {
+		for (int i = 0; i < m; ++i) cout << ans[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	for (int i = 0; i < n; ++i) {
+		if (visited[i]) continue;
+		visited[i] = 1;
+		ans[depth] = arr[i];
+		backt(depth + 1);
+		visited[i] = 0;
+	}
+	return;
+}
