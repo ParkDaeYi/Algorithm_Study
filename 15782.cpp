a82c08ca56214658a6091c6cbff78c9c53f653cf
ParkDaeYi <60999133+ParkDaeYi@users.noreply.github.com>
2020-08-25 17:56:26 +0900

    Calculate! 2

--- 15782.cpp
+++ 15782.cpp
@@ -1,91 +1,93 @@
 #include <iostream>
+#include <cstdio>
 #include <vector>
-#define MAX_N 100001
+#define N 100001
+#pragma warning(disable:4996)
 using namespace std;
 
-int n, m, seg[MAX_N * 4], lazy[MAX_N * 4], number[MAX_N], child[MAX_N], w[MAX_N], order[MAX_N], sz;
-vector<int> p[MAX_N];
-bool used[MAX_N];
+int n, m, arr[N], cnt[N], child[N], order[N], sz, seg[N * 4], lazy[N * 4];
+// cnt: 현재 정점 + 자식 개수, child: 자식
+vector<int> adj[N];
+bool used[N];
 
 int range(int idx) {
 	used[idx] = 1;
 	int ch = 0;
-	for (int nxt : p[idx]) {
+	for (int nxt : adj[idx]) {
 		if (used[nxt]) continue;
 		ch += range(nxt);
 	}
-	number[idx] = ++sz;
+	cnt[idx] = ++sz;
 	child[idx] = ch;
-	order[sz] = w[idx];
+	order[sz] = arr[idx];
 	return ch + 1;
 }
 
-void init(int node, int s, int e) {
-	if (s == e) {
-		seg[node] = order[s];
+void init(int node, int start, int end) {
+	if (start == end) {
+		seg[node] = order[start];
 		return;
 	}
-	int mid = (s + e) >> 1;
-	init(node * 2, s, mid);
-	init(node * 2 + 1, mid + 1, e);
+	int mid = (start + end) >> 1;
+	init(node * 2, start, mid);
+	init(node * 2 + 1, mid + 1, end);
 	seg[node] = seg[node * 2] ^ seg[node * 2 + 1];
 }
 
-void update_lazy(int node, int s, int e) {
+void update_lazy(int node, int start, int end) {
 	if (!lazy[node]) return;
-	seg[node] ^= lazy[node] * ((e - s + 1) % 2);
-	if (s != e) {
+	seg[node] ^= ((end - start + 1) % 2) * lazy[node];
+	if (start != end) {
 		lazy[node * 2] ^= lazy[node];
 		lazy[node * 2 + 1] ^= lazy[node];
 	}
 	lazy[node] = 0;
 }
 
-void update_range(int node, int s, int e, int left, int right, int val) {
-	update_lazy(node, s, e);
-	if (e < left || right < s) return;
-	if (left <= s && e <= right) {
-		seg[node] ^= val * ((e - s + 1) % 2);
-		if (s != e) {
+void update_range(int node, int start, int end, int left, int right, int val) {
+	update_lazy(node, start, end);
+	if (right < start || end < left) return;
+	if (left <= start && end <= right) {
+		seg[node] ^= ((end - start + 1) % 2) * val;
+		if (start != end) {
 			lazy[node * 2] ^= val;
 			lazy[node * 2 + 1] ^= val;
 		}
 		return;
 	}
-	int mid = (s + e) >> 1;
-	update_range(node * 2, s, mid, left, right, val);
-	update_range(node * 2 + 1, mid + 1, e, left, right, val);
+	int mid = (start + end) >> 1;
+	update_range(node * 2, start, mid, left, right, val);
+	update_range(node * 2 + 1, mid + 1, end, left, right, val);
 	seg[node] = seg[node * 2] ^ seg[node * 2 + 1];
 }
 
-int query(int node, int s, int e, int left, int right) {
-	update_lazy(node, s, e);
-	if (e < left || right < s) return 0;
-	if (left <= s && e <= right) return seg[node];
-	int mid = (s + e) >> 1;
-	return query(node * 2, s, mid, left, right) ^ query(node * 2 + 1, mid + 1, e, left, right);
+int query(int node, int start, int end, int left, int right) {
+	update_lazy(node, start, end);
+	if (right < start || end < left) return 0;
+	if (left <= start && end <= right) return seg[node];
+	int mid = (start + end) >> 1;
+	return query(node * 2, start, mid, left, right) ^ query(node * 2 + 1, mid + 1, end, left, right);
 }
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-
-	cin >> n >> m;
-	for (int i = 1, x, y;i < n;++i) {
-		cin >> x >> y;
-		p[x].push_back(y);
-		p[y].push_back(x);
+	//freopen("inp.inp", "r", stdin);
+	//freopen("out.out", "w", stdout);
+	scanf("%d %d\n", &n, &m);
+	for (int i = 1, a, b;i < n;++i) {
+		scanf("%d %d", &a, &b);
+		adj[a].push_back(b);
+		adj[b].push_back(a);
 	}
-	for (int i = 1;i <= n;++i) cin >> w[i];	
+	for (int i = 1;i <= n;++i) scanf("%d", arr + i);
 	range(1);
 	init(1, 1, n);
+	int a, b, c;
 	while (m--) {
-		int t, x, y;
-		cin >> t >> x;
-		if (t == 1) cout << query(1, 1, n, number[x] - child[x], number[x]) << '\n';
+		scanf("%d %d", &a, &b);
+		if (a == 1) printf("%d\n", query(1, 1, n, cnt[b] - child[b], cnt[b]));
 		else {
-			cin >> y;
-			update_range(1, 1, n, number[x] - child[x], number[x], y);
+			scanf(" %d", &c);
+			update_range(1, 1, n, cnt[b] - child[b], cnt[b], c);
 		}
 	}
 	return 0;
